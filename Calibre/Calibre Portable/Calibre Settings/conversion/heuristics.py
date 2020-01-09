b22473bb937d1a4ed608b9f652a970ff915490be
hantaotaohan <hantaotaohan@gmail.com>
2020-01-09 11:04:04 +0800

    add book

---
+++ Calibre/Calibre Portable/Calibre Settings/conversion/heuristics.py
@@ -0,0 +1,13 @@
+json:{
+  "delete_blank_paragraphs": true, 
+  "dehyphenate": true, 
+  "fix_indents": true, 
+  "replace_scene_breaks": "", 
+  "enable_heuristics": false, 
+  "markup_chapter_headings": true, 
+  "unwrap_lines": true, 
+  "format_scene_breaks": true, 
+  "renumber_headings": true, 
+  "html_unwrap_factor": 0.4, 
+  "italicize_common_cases": true
+}
